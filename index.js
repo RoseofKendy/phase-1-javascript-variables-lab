3ad8d9713e8b7fb57c8d01e2aad536f74b6b793d
Sharon Kendi <sharonkendi35@gmail.com>
2025-03-08 08:28:52 +0300

    Adds variables

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = "Chelsea"; // (2, 3) Declared as 'let' and assigned 'Chelsea'
+
+let companyCeo = "Susan Smith"; // (4, 5) Declared as 'let' and assigned 'Susan Smith'
